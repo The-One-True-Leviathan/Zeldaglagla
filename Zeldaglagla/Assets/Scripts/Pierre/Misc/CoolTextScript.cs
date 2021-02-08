7f1e986fcf1691668075cc59cc00c67070183c90
Pierre <p.cogne@rubika-edu.com>
2021-02-08 17:30:22 +0100

    Upgraded Cool Text Script !

--- Zeldaglagla/Assets/Scripts/Pierre/Misc/CoolTextScript.cs
+++ Zeldaglagla/Assets/Scripts/Pierre/Misc/CoolTextScript.cs
@@ -8,6 +8,8 @@ public class CoolTextScript : MonoBehaviour
     [SerializeField]
     string defaultText; 
     int currentChar = 0;
+    string currentText;
+    bool reading;
     Text text;
     // Start is called before the first frame update
     void Start()
@@ -23,21 +25,51 @@ public class CoolTextScript : MonoBehaviour
     public void Read(string textToRead)
     {
         StopAllCoroutines();
-        text.text = "";
         currentChar = 0;
-        char[] textToReaCharred = textToRead.ToCharArray();
-        StartCoroutine(ReadCoroutine(textToReaCharred));
+        if (reading)
+        {
+            StopAllCoroutines();
+            text.text = currentText;
+            reading = false;
+        }
+        else
+        {
+            currentText = textToRead;
+            text.text = "";
+            StartCoroutine(ReadCoroutine(textToRead.ToCharArray()));
+        }
+    }
+
+    public void Read(char[] textToRead)
+    {
+        StopAllCoroutines();
+        currentChar = 0;
+        if (reading)
+        {
+            text.text = currentText;
+            reading = false;
+        }
+        else
+        {
+            currentText = textToRead.ToString();
+            text.text = "";
+            StartCoroutine(ReadCoroutine(textToRead));
+        }
     }
 
     IEnumerator ReadCoroutine(char[] textToRead)
     {
         if (currentChar < textToRead.Length)
-        { 
+        {
+            reading = true;
             //SOUND TEXT BLEEP
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(0.075f);
             text.text += textToRead[currentChar];
             currentChar++;
             StartCoroutine(ReadCoroutine(textToRead));
+        } else
+        {
+            reading = false;
         }
 
     }

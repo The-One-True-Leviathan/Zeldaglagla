f66ebc973bea1ce5242e643dd46844cf4a613074
PALADIOUM <h.dontzow@rubika-edu.com>
2021-03-16 16:34:51 +0100

    H_007 : LA BAGARRE : Coup au piolet dans toutes les directions, "freeze frame" ajustable à chaque ennemi touché

--- Zeldaglagla/Assets/Scripts/HDO_Script/Character/HDO_CharacterCombat.cs
+++ Zeldaglagla/Assets/Scripts/HDO_Script/Character/HDO_CharacterCombat.cs
@@ -9,9 +9,18 @@ public class HDO_CharacterCombat : MonoBehaviour
     HDO_Torch torch;
     [SerializeField]
     HDO_HeatManager hm;
+    [SerializeField]
+    HDO_Piolet piolet;
 
     [Header("Combat Statistics")]
+    public bool canStrike;
     public int attackDamage;
+    [SerializeField]
+    float attackCooldown;
+    float elapsedTimeA;
+    [SerializeField]
+    Animator pioletAnim;
+    
 
 
     [Header("Torch Statistics")]
@@ -30,27 +39,66 @@ public class HDO_CharacterCombat : MonoBehaviour
 
         controls.UsualControls.Torch.performed += ctx => Torch();
         controls.KBControlsWASD.Torch.performed += ctx => Torch();
+
+        controls.UsualControls.Attack.performed += ctx => Strike();
+        controls.KBControlsWASD.Attack.performed += ctx => Strike();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(torchCDElapsed > 0)
+        {
+            torchCDElapsed -= Time.deltaTime;
+        }
+        else
+        {
+            torchCDElapsed = 0;
+        }
+
+        if (!canStrike)
+        {
+            elapsedTimeA -= Time.deltaTime;
+            if (elapsedTimeA <= 0)
+            {
+                elapsedTimeA = 0;
+                canStrike = true;
+            }
+        }
+
         
+    }
+
+    void Strike()
+    {
+        if (!canStrike)
+        {           
+            return;
+        }
+
+        Debug.Log("attack !");
+        pioletAnim.SetFloat("gunPointX", gunPoint.transform.localPosition.x);
+        pioletAnim.SetFloat("gunPointY", gunPoint.transform.localPosition.y);
+        piolet.ffed.Clear();
+        pioletAnim.SetTrigger("Attack");
+        canStrike = false;
+        elapsedTimeA = attackCooldown;
+
     }
 
     void Torch()
     {
-        if(hm.heatValue < torchHeatCost)
+        if(hm.heatValue < torchHeatCost || torchCDElapsed > 0)
         {
             return;
         }
         hm.heatValue -= torchHeatCost;
+        torchCDElapsed = torchCooldown;
 
         torch = Instantiate(shot, transform.position, Quaternion.identity).GetComponent<HDO_Torch>();
 
